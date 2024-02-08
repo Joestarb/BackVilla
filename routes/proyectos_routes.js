aefc8c02b9b7a54e1f26451b6a797a7c2165641c
Solrac619 <solracyestom2015@gmail.com>
2024-02-08 12:45:26 -0500

    correcion de rutas

--- routes/proyectos_routes.js
+++ routes/proyectos_routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const proyectoControllers = require('../controllers/proyectos_controllers');
 
-router.post('/', proyectoControllers.createProyecto);
-router.get('/:proyecto_id', proyectoControllers.getProyectoById);
-router.put('/:proyecto_id', proyectoControllers.updateProyecto);
-router.get('/', proyectoControllers.getAllProyectos);
-router.delete('/:proyecto_id', proyectoControllers.deleteProyecto);
+router.post('/proyecto', proyectoControllers.createProyecto);
+router.get('/proyecto/:proyecto_id', proyectoControllers.getProyectoById);
+router.put('/proyecto/:proyecto_id', proyectoControllers.updateProyecto);
+router.get('/proyecto', proyectoControllers.getAllProyectos);
+router.delete('/proyecto/:proyecto_id', proyectoControllers.deleteProyecto);
 
 module.exports = router;
